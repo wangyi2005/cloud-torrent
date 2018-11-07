34726538e73145baf7e7776355a5f8d03aaf3b90
wangyi2005 <wangyi2005@users.noreply.github.com>
2018-11-07 10:49:41 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,6 @@
-#cd /v2raybin
-#echo -e "$CONFIG_JSON" > config.json
-#if [ "$CERT_PEM" != "$KEY_PEM" ]; then
-#  echo -e "$CERT_PEM" > cert.pem
-#  echo -e "$KEY_PEM"  > key.pem
-#fi
 
 cd /ct
 wget -O http://github.com/jpillora/cloud-torrent/releases/download/$VER/cloud-torrent_linux_amd64.gz
+tar xfz cloud-torrent_linux_amd64.gz
 
-unzip v2ray.zip 
-if [ ! -f "v2ray" ]; then
-  mv /v2ray/v2ray-v$VER-linux-64/v2ray .
-  mv /v2ray/v2ray-v$VER-linux-64/v2ctl .
-  mv /v2ray/v2ray-v$VER-linux-64/geoip.dat .
-  mv /v2ray/v2ray-v$VER-linux-64/geosite.dat .
-fi
-
-cp -f /config.json .
-chmod +x v2ray v2ctl
-sed -i "s/your_uuid/$UUID/g" config.json
-./v2ray
+./cloud-torrent_linux_amd64 --port 8080
