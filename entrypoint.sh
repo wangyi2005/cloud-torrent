385f68d42cca2988e1e1a964bbd9b769cdf4a8bb
wangyi2005 <wangyi2005@users.noreply.github.com>
2018-11-07 11:20:09 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,2 @@
 
-cd /ct
-wget http://github.com/jpillora/cloud-torrent/releases/download/$VER/cloud-torrent_linux_amd64.gz
-tar xfz cloud-torrent_linux_amd64.gz
-
 ./cloud-torrent_linux_amd64 --port 8080
